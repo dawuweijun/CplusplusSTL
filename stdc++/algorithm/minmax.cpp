f835d048781215ab3ad58b463e1d465c56de6bbc
Liu Ming <miliu@statoil.com>
2014-05-24 21:19:25 +0800

    stdc++ <algorithm> examples.

---
+++ stdc++/algorithm/minmax.cpp
@@ -0,0 +1,11 @@
+// minmax example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::minmax
+
+int main () {
+    auto result = std::minmax({1,2,3,4,5});
+
+    std::cout << "minmax({1,2,3,4,5}): ";
+    std::cout << result.first << ' ' << result.second << '\n';
+    return 0;
+}
